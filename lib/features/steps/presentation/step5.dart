bf7dc69be94e136a98b97788f1276121880420cb
Ahmet Can Ogreten <ahmetcan.ogreten@kuartis.com>
2022-11-11 21:26:15 +0300

    Step 5 : Thank you

--- lib/features/steps/presentation/step5.dart
+++ lib/features/steps/presentation/step5.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_presentation/features/authentication/bloc/authentication_bloc.dart';
 
 class Step5 extends StatelessWidget {
   const Step5({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
-      builder: (context, state) {
-        if (state is LoggedIn) {
-          return const Center(child: Text('Step 2'));
-        } else {}
-        return const Center(child: Text('Not Logged In Yet'));
-      },
-    );
+    return Center(child: Image.asset('assets/bear.gif'));
   }
 }
